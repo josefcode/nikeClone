adb395ddaf03c5c34183c853222e325308b3a987
josefcode <atef.chela@gmail.com>
2023-01-12 21:42:53 -0300

    solve banner image

--- pages/components/slidePromotion/index.jsx
+++ pages/components/slidePromotion/index.jsx
@@ -11,8 +11,10 @@ export function SlidePromotion(){
         <p className={styles.small_text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
         
         </div>
-        <div>
-            <Image src = "https://images.unsplash.com/photo-1579758629938-03607ccdbaba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" width = {1200} height = {600} />
+        <div className= {styles.image_wrapper}>
+        <div className={styles.image_banner} >
+            <Image src = "https://images.unsplash.com/photo-1579758629938-03607ccdbaba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" alt ="image of banner" fill/>
+        </div>
         </div>
         </>
     )
