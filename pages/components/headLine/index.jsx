61cbc23cf19b9379becddd93db336545c3388b2a
Atef Chelaghma <atef.chelaghma@sovos.com>
2023-01-16 08:48:07 -0300

    update Headline style

--- pages/components/headLine/index.jsx
+++ pages/components/headLine/index.jsx
@@ -9,12 +9,24 @@ export default function HeadLine(){
             <div className= {styles.title}>
                 <h1>Featured Footwear for Her</h1>
             </div>
-           
+               <div className = {styles.cardContainerOne}>
                 <div className= {styles.head_image}>
                 <Image  className = {styles.imageOne} src = "https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="woman sport" fill />
+                
                 </div>
+                <div className= {styles.cardChild}>
+                <h3>Pegasus 39: <br/> In Peg We Trust</h3>
+                <button>Women's Running Footwear</button>
+                </div>
+                </div>
+                <div className = {styles.cardContainerTwo}>
                 <div className = {styles.head_image_two}>
                 <Image className = {styles.imageTwo} src = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3073&q=80" alt="woman sport2" fill />
+                </div>
+                <div className= {styles.cardChildTwo}>
+                <h3>Built to Power <br/> Your Progress</h3>
+                <button>Women's Running Footwear</button>
+                </div>
                 </div>
             
         </section>
