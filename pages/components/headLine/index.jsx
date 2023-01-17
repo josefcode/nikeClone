dc52076b81513330c5292aa7065a69f43806e954
Atef Chelaghma <atef.chelaghma@sovos.com>
2023-01-17 09:19:47 -0300

    add banner

--- pages/components/headLine/index.jsx
+++ pages/components/headLine/index.jsx
@@ -5,6 +5,7 @@ import styles from '../../../styles/HeadLine.module.css'
 
 export default function HeadLine(){
     return (
+        <>
         <section className= {styles.headContainer}>
             <div className= {styles.title}>
                 <h1>Featured Footwear for Her</h1>
@@ -15,8 +16,8 @@ export default function HeadLine(){
                 
                 </div>
                 <div className= {styles.cardChild}>
-                <h3>Pegasus 39: <br/> In Peg We Trust</h3>
-                <button>Women's Running Footwear</button>
+                <h3 className= {styles.bannerTitle} >Pegasus 39: <br/> In Peg We Trust</h3>
+                <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
                 </div>
                 <div className = {styles.cardContainerTwo}>
@@ -24,11 +25,38 @@ export default function HeadLine(){
                 <Image className = {styles.imageTwo} src = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3073&q=80" alt="woman sport2" fill />
                 </div>
                 <div className= {styles.cardChildTwo}>
-                <h3>Built to Power <br/> Your Progress</h3>
-                <button>Women's Running Footwear</button>
+                <h3 className= {styles.bannerTitle}>Built to Power <br/> Your Progress</h3>
+                <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
                 </div>
             
         </section>
+         <section className= {styles.headContainer}>
+         <div className= {styles.title}>
+             <h1>Featured Footwear for Her</h1>
+         </div>
+        
+            <div className= {styles.secondBanner}>
+                <Image src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" width = {1920} height = {300} alt = 'shoes' /> 
+                <div className= {styles.secondBannerBtn}>
+                <button className= {styles.btn}>Shop Drops</button>
+                </div>
+             </div>
+        
+     </section>
+     <section className= {styles.headContainer}>
+         <div className= {styles.title}>
+             <h1>Featured Footwear for Her</h1>
+         </div>
+        
+            <div className= {styles.secondBanner}>
+                <Image src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" width = {1920} height = {300} alt = 'shoes' /> 
+                <div className= {styles.secondBannerBtn}>
+                <button className= {styles.btn}>Shop Drops</button>
+                </div>
+             </div>
+        
+     </section>
+     </>
     )
 }
