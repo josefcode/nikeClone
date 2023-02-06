9da5c4b2fe036ab2abf15b6597e804f8dd2246a4
josefcode <atef.chela@gmail.com>
2023-02-05 22:40:34 -0300

    edit card style

--- pages/components/headLine/index.jsx
+++ pages/components/headLine/index.jsx
@@ -10,26 +10,26 @@ export default function HeadLine(){
             <div className= {styles.title}>
                 <h1>Featured Footwear for Her</h1>
             </div>
-               <div className = {styles.cardContainerOne}>
-                <div className= {styles.head_image}>
-                <Image  className = {styles.imageOne} src = "https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="woman sport" fill />
-                
-                </div>
-                <div className= {styles.cardChild}>
+            <div className= {styles.cardContainer}>
+               <div>
+                <Image  id = 'image1'className = {styles.imageOne} src = "https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="woman sport"  
+                fill/>
+        
+                <div >
                 <h3 className= {styles.bannerTitle} >Pegasus 39: <br/> In Peg We Trust</h3>
                 <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
                 </div>
-                <div className = {styles.cardContainerTwo}>
-                <div className = {styles.head_image_two}>
-                <Image className = {styles.imageTwo} src = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3073&q=80" alt="woman sport2" fill />
-                </div>
+                <div>
+                <Image id = "image2" className = {styles.imageTwo} src = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3073&q=80" alt="woman sport2" fill />
+                
                 <div className= {styles.cardChildTwo}>
                 <h3 className= {styles.bannerTitle}>Built to Power <br/> Your Progress</h3>
                 <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
                 </div>
-            
+                </div>
+                
         </section>
          <section className= {styles.headContainer}>
          <div className= {styles.title}>
