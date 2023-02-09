2f074c53ba2a12874f7c9d2ab8e4541a92c65640
josefcode <atef.chela@gmail.com>
2023-02-09 08:55:18 -0300

    clean image style

--- pages/components/card/index.js
+++ pages/components/card/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
+import styles from '../../../styles/Cards.module.css'
 
 export default function Card({image, title, description, alt}) {
   return (
-    <div>
-        <Image  src = {image} alt = {alt} width = {350} height = {350} />
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
+    <>
+    <div className= {styles.cardContainer}>
+        <Image className= {styles.cardImage} src = {image} alt = {alt} fill />
+     
     </div>
+       <div>
+       <h2>{title}</h2>
+       <p>{description}</p>
+   </div>
+   </>
   )
 }
--- pages/components/headLine/index.jsx
+++ pages/components/headLine/index.jsx
@@ -11,19 +11,19 @@ export default function HeadLine(){
                 <h1>Featured Footwear for Her</h1>
             </div>
             <div className= {styles.cardContainer}>
-               <div>
+               <div className={styles.imageOneContainer}>
                 <Image  id = 'image1'className = {styles.imageOne} src = "https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="woman sport"  
                 fill/>
         
-                <div >
+                <div className= {styles.cardOne}>
                 <h3 className= {styles.bannerTitle} >Pegasus 39: <br/> In Peg We Trust</h3>
                 <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
                 </div>
-                <div>
+                <div className={styles.imageTwoContainer}>
                 <Image id = "image2" className = {styles.imageTwo} src = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3073&q=80" alt="woman sport2" fill />
                 
-                <div className= {styles.cardChildTwo}>
+                <div className= {styles.cardTwo}>
                 <h3 className= {styles.bannerTitle}>Built to Power <br/> Your Progress</h3>
                 <button className= {styles.btn}>Women's Running Footwear</button>
                 </div>
@@ -31,14 +31,16 @@ export default function HeadLine(){
                 </div>
                 
         </section>
+
+
          <section className= {styles.headContainer}>
          <div className= {styles.title}>
              <h1>Featured Footwear for Her</h1>
          </div>
         
-            <div className= {styles.secondBanner}>
-                <Image src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" width = {1920} height = {300} alt = 'shoes' /> 
-                <div className= {styles.secondBannerBtn}>
+            <div className= {styles.featureBannerContainer}>
+                <Image className = {styles.featureBanner} src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt = 'shoes' fill/> 
+                <div className= {styles.featureBannerBtn}>
                 <button className= {styles.btn}>Shop Drops</button>
                 </div>
              </div>
@@ -49,9 +51,9 @@ export default function HeadLine(){
              <h1>Featured Footwear for Her</h1>
          </div>
         
-            <div className= {styles.secondBanner}>
-                <Image src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" width = {1920} height = {300} alt = 'shoes' /> 
-                <div className= {styles.secondBannerBtn}>
+            <div className= {styles.featureBannerContainer}>
+                <Image className = {styles.featureBanner} src = "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt = 'shoes' fill /> 
+                <div className= {styles.featureBannerBtn}>
                 <button className= {styles.btn}>Shop Drops</button>
                 </div>
              </div>
